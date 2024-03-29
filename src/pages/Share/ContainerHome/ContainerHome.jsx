5db8ca9d8b6925677080b558693d6528584bcea9
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2024-03-29 08:23:54 +0330

    revise

--- src/pages/Share/ContainerHome/ContainerHome.jsx
+++ src/pages/Share/ContainerHome/ContainerHome.jsx
@@ -167,24 +167,24 @@ export default function ContainerHome() {
         </div>
       </section>
       <section className="newIn">
-        <h2 className="newIn-title mb-0 text-center text-white d-none d-md-block">
+        <h2 className="newIn-title  text-center text-white d-none d-md-block">
           New In
         </h2>
         <div className="newIn-body">
           <div className="row gx-5">
-            <div className=" col-12 col-md-6 ">
-              <div className="h-100  mx-auto">
-                <h2 className="newIn-firstSlide-title-mobile d-md-none">
+            <div className=" col-12 col-md-6 text-center firstSlide py-5 py-md-0 ">
+              <div className="h-100  ">
+                <h2 className="newIn-title  text-center text-white d-md-none mb-4">
                   New In
                 </h2>
-                {/* <div className="newIn-firstSlide-img">
-                    <img src="./public/images/faces/woman.png" alt="woman" />
-                  </div>  */}
-                <div className="newIn-firstSlide-table">
-                  <h3 className="newIn-firstSlide-desc-title">
+                <div className="newIn-firstSlide-img mb-5">
+                  <img src="./public/images/faces/woman.png" alt="woman" />
+                </div>
+                <div className="newIn-firstSlide">
+                  <h3 className="newIn-firstSlide-title fs-2">
                     Beautya La Mousse Off/On Foaming Cleaner
                   </h3>
-                  <p className="newIn-firstSlide-desc">
+                  <p className="newIn-firstSlide-desc text-white fs-4">
                     Amet minim mollit non deserunt ullamco est sit aliqua dolor
                     do amet sint. Velit officia consequat duis en im velit
                     mollit.
@@ -196,41 +196,43 @@ export default function ContainerHome() {
               <div className="h-100">
                 <div className="row gx-5">
                   <div className=" col-12 col-md-6 my-5 mt-md-0">
-                    <div className="h-100 product-inner">
-                      <div className="newIn-product-img">
+                    <div className="h-100 border border-1 product-inner py-5">
+                      <div className="newIn-product-img text-center ">
                         <img
                           src="./public/images/newIn/product image.png"
                           alt="product1"
                         />
                       </div>
-                      <div className="newIn-product-table">
-                        <h3 className="newIn-firstSlide-desc-title ">
+                      <div className="newIn-product-desc text-center text-md-start px-3 py-3 ">
+                        <h3 className="newIn-product-desc-title  ">
                           Beautya Capture Total Dreamskin Care & Perfect
                         </h3>
-                        <p className="newIn-firstSlide-desc ">
+                        <p className="newIn-firstSlide-desc fs-5 ">
                           Plumping Gloss - Instant and Long-Term Volume Effect -
                           24h Hydration
                         </p>
-                        <span className="newIn-price">$76.00</span>
+                        <span className="newIn-price fs-3">$76.00</span>
                       </div>
                     </div>
                   </div>
                   <div className="newIn-product col-12 col-md-6 my-5 mt-md-0">
-                    <div className="h-100 ">
-                      <div className="newIn-product-img ">
+                    <div className="h-100 product-inner border border-1 py-5">
+                      <div className="newIn-product-img text-center ">
                         <img
                           src="./public/images/newIn/product image2.png"
                           alt="product1"
                         />
                       </div>
-                      <h3 className="newIn-firstSlide-desc-title ">
-                        Beautya Capture Total Dreamskin Care & Perfect
-                      </h3>
-                      <p className="newIn-firstSlide-desc ">
-                        Plumping Gloss - Instant and Long-Term Volume Effect -
-                        24h Hydration
-                      </p>
-                      <span className="newIn-price">$70.00</span>
+                      <div className="newIn-product-desc text-center text-md-start px-3 py-3">
+                        <h3 className="newIn-product-desc-title  ">
+                          Beautya Capture Total Dreamskin Care & Perfect
+                        </h3>
+                        <p className="newIn-firstSlide-desc fs-5 ">
+                          Plumping Gloss - Instant and Long-Term Volume Effect -
+                          24h Hydration
+                        </p>
+                        <span className="newIn-price fs-3">$70.00</span>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -239,7 +241,30 @@ export default function ContainerHome() {
           </div>
         </div>
       </section>
-      {/* <section className="offer-desktop ">
+      <section className="offer-desktop ">
+        <div className="row gx-5  m-0">
+          <div className="offer-desktop-img col-12 col-md-6 p-0">
+            <img src="./public/images/offer-desktop.png" alt="offer" />
+          </div>
+          <div className="offer-desktop-detail col-12 col-md-6 p-0">
+            <h4 className="offer-desktop-detail-title1">Special offers</h4>
+            <h3 className="offer-desktop-detail-title2">Save up to 50%</h3>
+            <p className="offer-desktop-detail-desc1">
+              Mother’s Day is coming! For everything she’s given you, it's time
+              to give back. Shower her with love, happiness, and the best of
+              Beautya.
+            </p>
+            <p className="offer-desktop-detail-desc2">
+              visit your local beautya branches to find out more about our
+              special offers in make up and skincare products.
+            </p>
+            <button className="offer-desktop-detail-btn">
+              <a href="#">find branches</a>
+            </button>
+          </div>
+        </div>
+      </section>
+      {/*  <section className="offer-desktop ">
         <div className="offer-desktop-img">
           <img src="./public/images/offer-desktop.png" alt="offer" />
         </div>
@@ -283,7 +308,7 @@ export default function ContainerHome() {
         <div className="analysis-desktop-img">
           <img src="./public/images/faces/faces-desktop.png" alt="faces" />
         </div>
-      </section> */}
+      </section>
       <section className="blog-desktop">
         <h2 className="blog-desktop-title">Our Blog</h2>
         <div className="blog-desktop-container">
@@ -452,7 +477,7 @@ export default function ContainerHome() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
     </div>
   );
 }
