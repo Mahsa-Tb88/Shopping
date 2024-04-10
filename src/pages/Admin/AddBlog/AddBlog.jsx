124e887ab84b3928c363384f2e11e61c81c4ba3b
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2024-04-09 22:32:00 -0700

    revise

--- src/pages/Admin/AddBlog/AddBlog.jsx
+++ src/pages/Admin/AddBlog/AddBlog.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect } from "react";
 import FormBlog from "../FormBlog/FormBlog";
+import { Helmet } from "react-helmet";
 export default function AddBlog() {
-  useEffect(() => {
-    document.title = "New Blog";
-  }, []);
-
   return (
     <div className="addProduct px-2 px-md-4 py-5">
+      <Helmet>New Blog</Helmet>
       <h1>Create New Blog</h1>
       <FormBlog type="new" />
     </div>
